7e09920b3c42c03b171409c17a5e532de7eecf8b
Mathe <matheusamrl@gmail.com>
2023-03-15 14:42:17 -0300

    Api GW with load balancer

---
+++ src/main/java/com/matheus/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.matheus.apigateway;
+
+import org.apache.http.client.HttpClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+
+}
---
+++ src/main/java/com/matheus/apigateway/LogginFilter/LogginFilter.java
@@ -0,0 +1,19 @@
+package com.matheus.apigateway.LogginFilter;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.logging.Logger;
+
+public class LogginFilter implements GlobalFilter {
+
+    private Logger logger = (Logger) LoggerFactory.getLogger(LogginFilter.class);
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Original request path - > {}");
+        return chain.filter(exchange);
+    }
+}
